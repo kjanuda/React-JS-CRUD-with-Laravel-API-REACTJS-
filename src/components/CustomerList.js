22024d8e7a1434b7d3f5b557033b3ae9f973c9d4
kjanuda <113584734+kjanuda@users.noreply.github.com>
2025-01-26 21:43:31 +0530

    Update CustomerList.js

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -29,12 +29,7 @@ class CustomerList extends Component {
           <tbody>
             {customers.map(customer => {
               return (
-                <Customer
-                  key={customer.id}
-                  customer={customer}
-                  onDelete={this.onDelete}
-                  onEdit={this.onEdit}
-                />
+                
               );
             })}
           </tbody>
